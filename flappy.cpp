d213531c073811060ac7050b35e1169df680ae87
Diki Bagastama <dikibagas139@gmail.com>
2021-11-21 17:07:20 +0700

    assembling parts

--- flappy.cpp
+++ flappy.cpp
@@ -6,95 +6,125 @@
 #include <string>
 #include <iomanip>
 #include <algorithm>
+#include <cwchar>
 
 using namespace std;
 
-
-void swap(char &a, char &b);
+void swap(char& a, char& b);
 void fullscreen();
 void decoration();
 void spaces(string str);
 void intro();
 void menu();
-void game();
-void map(vector <vector<char>> &area, int &j, int &k, int &playerI, int &playerJ, int &score);
+void game(bool player);
+void map(vector <vector<char>>& area, int& j, int& k, int& playerI, int& playerJ, int& score);
 void pixel();
 int randomGenerator();
 void leaderboard();
 void input_leaderboard(string& name, int& score);
+void player();
+void credits();
+void instruction();
 
-// MISSING LINKs
-// 1. Guest
-// 2. Player --> Save Score
-// 3. Difficulty Mechanism
-// Wrapping up
-// 1. Finishing Intro
-// 2. Adding Options(?)
-// 3. Finishing Exit
-// 4. Credits
-// 5. Terminal Colors
-// 6. Resizing Interface
-
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
     fullscreen();
     intro();
-    // game();
     return 0;
 }
 
-void swap(char &a, char &b){
+void swap(char& a, char& b) {
     char temp;
     temp = a;
     a = b;
     b = temp;
 }
 
-void fullscreen(){
+void fullscreen() {
     system("mode con COLS=700");
-    ShowWindow(GetConsoleWindow(),SW_MAXIMIZE);
-    SendMessage(GetConsoleWindow(),WM_SYSKEYDOWN,VK_RETURN,0x20000000);
+    ShowWindow(GetConsoleWindow(), SW_MAXIMIZE);
+    SendMessage(GetConsoleWindow(), WM_SYSKEYDOWN, VK_RETURN, 0x20000000);
+    CONSOLE_FONT_INFOEX cfi;
+    cfi.cbSize = sizeof(cfi);
+    cfi.nFont = 0;
+    cfi.dwFontSize.X = 0;                   // Width of each character in the font
+    cfi.dwFontSize.Y = 32;                  // Height
+    cfi.FontFamily = FF_DONTCARE;
+    cfi.FontWeight = FW_NORMAL;
+    std::wcscpy(cfi.FaceName, L"Consolas"); // Choose your font
+    SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
 }
 
-void spaces(string str = ""){
-    int length = 91-(str.length()/4);
-    for(int i=0; i<length; i++){
+void spaces(int len = 48) {
+    string str = "" ;
+    int length = len - (str.length() / 4);
+    for (int i = 0; i < length; i++) {
         cout << " ";
     }
 }
 
-void intro(){
-    int j=0;
-    int k=0;
+void instruction(){
+    system("cls");
+    char ch;
+    decoration();
+    spaces();
+    cout << "          HOW TO PLAY" << endl;
+    decoration();
+
+    spaces();
+    cout << "# GAME" << endl;
+    spaces();
+    cout << "-Avoid obstacles" << endl;
+    spaces();
+    cout << "-Press 'W' to go up" << endl;
+    cout << endl;
+    spaces();
+    cout << "# MENU" << endl;
+    spaces();
+    cout << "-Guest  : Scores is not saved" << endl;
+    spaces();
+    cout << "-Player : Scores is saved" << endl;
+
+    decoration();
+    spaces();
+    cout << "           HAVE FUN!";
+    ch = _getch();
+}
+
+void intro() {
+    int j = 0;
+    int k = 0;
     char ch;
     vector<char> welcome = {'W','E','L','C','O','M','E',
-                            ' ','T','O',' ','F','L','A',
+                            ' ','T','O',' ','X','-','H',
+                            'I','T',' ','(','F','L','A',
                             'P','P','Y',' ','B','I','R',
                             'D',' ','R','E','P','L','I',
-                            'C','A','T','I','O','N',' ',
-                            ' '};
-    while(true){
+                            'C','A','T','I','O','N',')',
+                            ' ',' ',};
+    while (true) {
         system("cls");
         decoration();
         spaces();
-        for(j; j<k+31; j++){
+        for (j; j < k + 31; j++) {
             cout << welcome[j];
         }
         cout << endl;
         decoration();
 
         k++;
-        j=k;
-        welcome.push_back(welcome[k-1]);
+        j = k;
+        welcome.push_back(welcome[k - 1]);
 
         cout << endl;
         spaces();
         cout << "PRESS ANY KEY TO ENTER THE GAME" << endl;
         cout << endl;
         decoration();
-
-        if(_kbhit()){
+        
+        if (_kbhit()) {
             ch = _getch();
+            instruction();
             menu();
             break;
         }
@@ -102,15 +132,16 @@ void intro(){
     }
 }
 
-void menu(){
+void menu() {
     string menu = "MAIN MENU";
-    int distance = 30-(menu.length()*2);
+    int distance = 30 - (menu.length() * 2);
     string str = "Thanks for your visit, hope you have a nice day!";
     int strLen = str.length();
-    while(true){
+    bool player;
+    while (true) {
         system("cls");
         decoration();
-        for(int i=1; i<distance; i++){
+        for (int i = 1; i < distance; i++) {
             cout << " ";
         }
         spaces();
@@ -128,58 +159,99 @@ void menu(){
         cout << "5. Exit" << endl;
         decoration();
         char choice = _getch();
-        switch (choice){
-            case '1':
-                // 1. Guest
-                game();
-                continue;
-            case '2':
-                // 2. Player
-                // game();
-                continue;
-            case '3':
-                // 3. Leaderboard();
-                leaderboard();
-                continue;
-            case '4':
-                // 4. Credits();
-                continue;
-            case '5':
-                system("cls");
-                spaces();
-                pixel();
-                spaces();
-                for(int i=0; i<strLen; i++){
-                    cout << "=";
+        char color = 1;
+        switch (choice) {
+        case '1':
+            // 1. Guest
+            player = false;
+            game(player);
+            continue;
+        case '2':
+            // 2. Player
+            player = true;
+            game(player);
+            continue;
+        case '3':
+            // 3. Leaderboard();
+            leaderboard();
+            continue;
+        case '4':
+            credits();
+            // 4. Credits();
+            continue;
+        case '5':
+            system("cls");
+            spaces(40);
+            pixel();
+            spaces(40);
+            for (int i = 0; i < strLen; i++) {
+                cout << "=";
+            }
+            cout << endl;
+            spaces(40);
+            cout << str << endl;
+            spaces(40);
+            for (int i = 0; i < strLen; i++) {
+                cout << "=";
+            }
+
+            while(true) {
+                if (color == 9) color = 1;
+                string c = "color " + to_string(color);
+                system(c.c_str());
+                Sleep(500);
+                if(_kbhit()) {
+                    break;
                 }
-                cout << endl;
-                spaces();
-                cout << str << endl;
-                spaces();
-                for(int i=0; i<strLen; i++){
-                    cout << "=";
-                }
-                getchar();
-                break;
-            default:
-                continue;
+                color++;
+            }
+            break;
+        default:
+            continue;
         }
         break;
     }
 }
 
-void map(vector <vector<char>> &area, int &j, int &k, int &playerI, int &playerJ, int &score) {
+void credits () {
+    string arr[5][100] = {
+        {"Created By"},
+        {"Azzakhruf Aryan Nur Ihsan"},
+        {"Diki Bagastama"},
+        {"Kenneth Christopher Prasetya"},
+        {"Nur Aziz Tri Indrawan"},
+    };
     system("cls");
-    for(int i=0; i<12; i++){
+    decoration();
+    for(int j=0;j<5;j++){
+        j == 0 ? spaces(59) : spaces(50);
+        for (int i= 0; i < 50; i++) {
+            cout<< arr[j][i];
+        }
+        cout << endl;
+        for(int i=0; i< 10; i++) {
+            if(_kbhit()) {
+                return;
+            }
+            Sleep(100);
+        }
+    }
+    decoration();
+    _getch();
+}
+
+void map(vector <vector<char>>& area, int& j, int& k, int& playerI, int& playerJ, int& score) {
+    system("cls");
+    for (int i = 0; i < 12; i++) {
         spaces();
-        for(j; j<k+31; j++){
-            if(area[i][j]=='X'){
-                playerI=i;
-                playerJ=j;
+        for (j; j < k + 31; j++) {
+            if (area[i][j] == 'X') {
+                playerI = i;
+                playerJ = j;
             }
             cout << area[i][j];
         }
-        j=k;
+        j = k;
         cout << endl;
     }
     spaces();
@@ -188,56 +260,76 @@ void map(vector <vector<char>> &area, int &j, int &k, int &playerI, int &playerJ
     cout << "===============================" << endl;
 }
 
-void game(){
+void game(bool player) {
+    string name;
+    if(player){
+        spaces();
+        cout << "        INPUT YOUR NAME" << endl;
+        spaces();
+        cin >> name;
+    }
     repeat:
     srand(time(0));
     int playerI = 0, playerJ = 0;
-    int k=0, j=0;
+    int k = 0, j = 0;
     int score = 0;
     bool lose = false;
 
     vector <int> random;
-    for(int i=0; i<10000; i++){
-        random.push_back(rand()%12+1);
+    for (int i = 0; i < 10000; i++) {
+        random.push_back(rand() % 12 + 1);
     }
 
     // 5. Random Obstacles
     vector <vector<char>> area;
-    for (int i = 0; i < 12;i++) {
+    for (int i = 0; i < 12; i++) {
         area.push_back(vector<char>());
-        for(int j = 0; j < 10000; j++) {
-            if(i==0 || i==11){
+        for (int j = 0; j < 10000; j++) {
+            if (i == 0 || i == 11) {
                 area[i].push_back('=');
-            }else if(i==5 && j==1){
+            }
+            else if (i == 5 && j == 1) {
                 area[i].push_back('X');
-            }else if(j==0){
-                area[i].push_back(' ');
-            }else{
+            }
+            else if (j == 0) {
                 area[i].push_back(' ');
             }
-            if(j%4==0 && j>1){
-                if(i>0 && i<11){
-                    if(random[j] == 1){
-                        if(i<2 || i>5) area[i].push_back('|');
-                    }else if(random[j] == 2){
-                        if(i>3) area[i].push_back('|');
-                    }else if(random[j] == 3){
-                        if(i<4 || i>7) area[i].push_back('|');
-                    }else if(random[j] == 4){
-                        if(i<6) area[i].push_back('|');
-                    }else if(random[j] == 5){
-                        if(i<3 || i>6) area[i].push_back('|');
-                    }else if(random[j] == 6){
-                        if(i>3 && i<7) area[i].push_back('|');
-                    }else if(random[j] == 7){
-                        if(i>5 && i<9) area[i].push_back('|');
-                    }else if(random[j] == 8){
-                        if(i<5) area[i].push_back('|');
-                    }else if(random[j] == 9){
-                        if(i>2 && i<8) area[i].push_back('|');
-                    }else if(random[j] == 10){
-                        if(i>4) area[i].push_back('|');
-                    }else{
+            else {
+                area[i].push_back(' ');
+            }
+            if (j % 4 == 0 && j > 1) {
+                if (i > 0 && i < 11) {
+                    if (random[j] == 1) {
+                        if (i < 2 || i>5) area[i].push_back('|');
+                    }
+                    else if (random[j] == 2) {
+                        if (i > 3) area[i].push_back('|');
+                    }
+                    else if (random[j] == 3) {
+                        if (i < 4 || i>7) area[i].push_back('|');
+                    }
+                    else if (random[j] == 4) {
+                        if (i < 6) area[i].push_back('|');
+                    }
+                    else if (random[j] == 5) {
+                        if (i < 3 || i>6) area[i].push_back('|');
+                    }
+                    else if (random[j] == 6) {
+                        if (i > 3 && i < 7) area[i].push_back('|');
+                    }
+                    else if (random[j] == 7) {
+                        if (i > 5 && i < 9) area[i].push_back('|');
+                    }
+                    else if (random[j] == 8) {
+                        if (i < 5) area[i].push_back('|');
+                    }
+                    else if (random[j] == 9) {
+                        if (i > 2 && i < 8) area[i].push_back('|');
+                    }
+                    else if (random[j] == 10) {
+                        if (i > 4) area[i].push_back('|');
+                    }
+                    else {
                         area[i].push_back(' ');
                     }
                 }
@@ -245,31 +337,34 @@ void game(){
         }
     }
 
-    while(true){
+    while (true) {
         map(area, j, k, playerI, playerJ, score);
         char key;
         // 7. Difficulty Mechanism
-        Sleep(500);
+        Sleep(500 - score);
 
-        if(_kbhit()){
+        if (_kbhit()) {
             key = _getch();
-            if ((key=='w' || key=='W') && (playerI>0)){
-                if(area[playerI - 1][playerJ + 1] == '|') lose = true;
-                swap(area[playerI][playerJ],area[playerI--][playerJ++]);
-                if(area[playerI + 1][playerJ + 1] == '|' && area[playerI][playerJ + 1] == '|') lose = true;
-            }else{
-                if(area[playerI + 1][playerJ + 1] != ' ') lose = true;
-                swap(area[playerI][playerJ],area[playerI++][playerJ++]);
+            if ((key == 'w' || key == 'W') && (playerI > 0)) {
+                if (area[playerI - 1][playerJ + 1] == '|') lose = true;
+                swap(area[playerI][playerJ], area[playerI-1][playerJ+1]);
+                if (area[playerI + 1][playerJ + 1] == '|' && area[playerI][playerJ + 1] == '|') lose = true;
             }
-        }else{
-            if(area[playerI + 1][playerJ + 1] != ' ') lose = true;
-            swap(area[playerI][playerJ],area[playerI++][playerJ++]);
+        }
+        else {
+            if (area[playerI + 1][playerJ + 1] != ' ') lose = true;
+            swap(area[playerI][playerJ], area[playerI+1][playerJ+1]);
         }
 
-        if(lose == true){
+        if (lose == true) {
             char again;
-            // SAVE SCORE
-            while(true){
+            if(player){
+                ofstream outfile;
+                outfile.open("leaderboard.txt", std::ofstream::ate| std::ofstream::app);
+                outfile << name << " " << score;
+                outfile.close();
+            }
+            while (true) {
                 system("cls");
                 spaces();
                 cout << "=================" << endl << endl;
@@ -285,60 +380,60 @@ void game(){
                 cout << "Play Again? [Y/N]: ";
                 again = _getch();
 
-                if(again=='y' || again=='Y'){
+                if (again == 'y' || again == 'Y') {
                     goto repeat;
-                }else if(again=='n' || again=='N'){
+                }
+                else if (again == 'n' || again == 'N') {
                     break;
-                }else{
+                }
+                else {
                     continue;
                 }
             }
             break;
         }
-
         score++;
         k++;
-        j=k;
+        j = k;
     }
 }
 
-void decoration(){
+void decoration() {
     spaces();
-    for(int i=0; i<31; i++){
-            cout << "=";
+    for (int i = 0; i < 31; i++) {
+        cout << "=";
     }
     cout << endl;
 }
 
-void pixel(){
+void pixel() {
     cout << endl;
-    spaces();
+    spaces(38);
     cout << "              ##################" << endl;
-    spaces();
+    spaces(38);
     cout << "           ###           #        #" << endl;
-    spaces();
-    cout << "          #            #            #" << endl; 
-    spaces(); 
+    spaces(38);
+    cout << "          #            #            #" << endl;
+    spaces(38);
     cout << "     #########           #             #" << endl;
-    spaces(); 
+    spaces(38);
     cout << "   #          #             #######    #" << endl;
-    spaces();
+    spaces(38);
     cout << " #             #                   ##############" << endl;
-    spaces();
+    spaces(38);
     cout << "   #          #                 #                 #" << endl;
-    spaces();
+    spaces(38);
     cout << "    #########               #	   #############" << endl;
-    spaces();
+    spaces(38);
     cout << "     #                         ##              #" << endl;
-    spaces();
+    spaces(38);
     cout << "       #####                      #    ########" << endl;
-    spaces(); 
-    cout << "            ####################" << endl;  
+    spaces(38);
+    cout << "            ####################" << endl;
     cout << endl;
 }
 
 void leaderboard() {
-
     struct player {
         string name;
         int score;
@@ -378,9 +473,8 @@ void leaderboard() {
     decoration();
     for (int i = 0; i < count; i++) {
         spaces();
-        // cout << i + 1 << ". " << p[i].name << right << setw(15) << p[i].score << endl;
         cout << i + 1 << ".\t" << p[i].name << "\t\t" << p[i].score << endl;
-        if(i == 9){
+        if (i == 9) {
             break;
         }
     }
@@ -388,16 +482,4 @@ void leaderboard() {
     spaces();
     cout << "Press any key to continue...";
     _getch();
-}
-
-// back-up
-void input_leaderboard(string& name, int& score) {
-    fstream myfile;
-    myfile.open("Leaderboard.txt", ios::app);
-    if (!myfile.fail()) {
-        cerr << "Error Membuka File" << endl;
-        exit(1);
-    }
-    myfile << name << " " << score << endl;
-    myfile.close();
 }
